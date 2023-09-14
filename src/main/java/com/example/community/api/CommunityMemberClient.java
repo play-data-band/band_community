8c0664355f816dd4696b8fc92f53504799d1934a
dokyeong99 <dlehrud451@gmail.com>
2023-09-14 15:35:33 +0900

    a

--- src/main/java/com/example/community/api/CommunityMemberClient.java
+++ src/main/java/com/example/community/api/CommunityMemberClient.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("COMMUNITYMEMBER-SERVICE")
 public interface CommunityMemberClient {
+
     @PutMapping("api/v1/communitymember/updatecommunity/{communityId}")
     void updateCommunityInCommunityMember(
             @RequestBody CommunityMemberReqeust communityMemberReqeust,
             @PathVariable("communityId") Long communityId);
+
 }
 
 
